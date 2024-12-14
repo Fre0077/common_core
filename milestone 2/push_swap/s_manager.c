3be1ed877ecbe1533845d22253cf6818034105f7
Fre0077 <161849028+Fre0077@users.noreply.github.com>
2024-12-14 14:44:42 +0100

    completata s_manager (no controllo)
    finito le due funzione per la gestione dell'arry s. nessun controllo effettuato. verificarne il funzionamento appena possibile.

--- milestone 2/push_swap/s_manager.c
+++ milestone 2/push_swap/s_manager.c
@@ -12,6 +12,7 @@
 
 #include "index.h"
 
+//trova il segmento di numeri ordinanato decrescente in a e lo restitiusce
 t_stack	*find_segment(t_stack *a)
 {
 	int		len;
@@ -38,16 +39,45 @@ t_stack	*find_segment(t_stack *a)
 	return (segment);
 }
 
-int	set_s(int **s, t_stack *a, t_stack *b)
+//imposta i valoridi a e b, inoltre mette a 0 si il need che il direct oltre mpostare il valore massimo
+int	set_s(int **s, t_stack *a)
 {
+	a = find_segment(a);
+	if (ft_listsize(a) ==  1)
+	{
+		s[0] = 50;
+		s[1] = 50;
+	}
+	else
+	{
+		s[0] = a->val;
+		while ((a->next)->val == a->val + 1)
+			a = a->next;
+		s[1] = a->val;
+	}
 	s[2] = 1;
 	s[3] = 1;
 	s[4] = ft_lstsize(a);
+	s[5] = 0;
 	s[6] = 0;
-	a = find_segment(a);
 }
 
+// aggiorna i valori del massimo e minimo di a e b
 void	upgrade_s(int **s, t_stack *a, t_stack *b)
 {
-	
+	a = find_segment(a);
+	if (ft_listsize(a) ==  1)
+	{
+		s[0] = 50;
+		s[1] = 50;
+	}
+	else
+	{
+		s[0] = a->val;
+		while ((a->next)->val == a->val + 1)
+			a = a->next;
+		s[1] = a->val;
+	}
+	s[2] = b->val;
+	s[3] = (b->previus)->val;
 }
